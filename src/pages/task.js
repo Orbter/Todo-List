495ec21c765373569649b38580a857dbdf2a2581
Orbter <assouline39@gmail.com>
2024-02-18 20:30:29 +0200

    finish the css for the task function need to add the svg

--- src/pages/task.js
+++ src/pages/task.js
@@ -1,5 +1,5 @@
 function createTask(name, owner, status, priority, tags, date) {
-    //Creating the divs
+  //Creating the divs
   const taskName = document.createElement("div");
   const taskOwner = document.createElement("div");
   const taskStatus = document.createElement("div");
@@ -8,11 +8,59 @@ function createTask(name, owner, status, priority, tags, date) {
   const taskDate = document.createElement("div");
   const taskEdit = document.createElement("div");
   const taskDelete = document.createElement("div");
-    
 
+  //classes
+  updateTaskClass(
+    taskStatus,
+    taskPriority,
+    taskName,
+    taskOwner,
+    taskTags,
+    taskDate,
+    taskEdit,
+    taskDelete
+  );
 }
-const decidingClass(taskStatus,taskPriority ) =>(){
-    if(taskStatus.inerText === "done"){
+const updateTaskClass = (
+  taskStatus,
+  taskPriority,
+  taskName,
+  taskOwner,
+  taskTags,
+  taskDate,
+  taskEdit,
+  taskDelete
+) => {
+  taskStatus.classList.remove(
+    "task-status-done",
+    "task-status-stuck",
+    "task-status-working"
+  );
+  taskPriority.classList.remove(
+    "task-priority-high",
+    "task-priority-low",
+    "task-priority-medium"
+  );
 
-    }
-}
+  if (taskStatus.innerText === "done") {
+    taskStatus.classList.add("task-status-done", "task-status");
+  } else if (taskStatus.innerText === "stuck") {
+    taskStatus.classList.add("task-status-stuck", "task-status");
+  } else {
+    taskStatus.classList.add("task-status-working", "task-status");
+  }
+
+  if (taskPriority.innerText === "high") {
+    taskPriority.classList.add("task-priority-high", "task-priority");
+  } else if (taskPriority.innerText === "low") {
+    taskPriority.classList.add("task-priority-low", "task-priority");
+  } else {
+    taskPriority.classList.add("task-priority-medium", "task-priority");
+  }
+  taskName.classList.add("task-name");
+  taskOwner.classList.add("task-owner");
+  taskTags.classList.add("task-tags");
+  taskDate.classList.add("task-date");
+  taskEdit.classList.add("task-edit", "bx-box-task");
+  taskDelete.classList.add("task-delete", "bx-box-task");
+};
